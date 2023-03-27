498ed249f8ef81fc3255904af8a9b0724ae00ecc
ArsPetr <arseniyp1@yandex.ru>
2023-03-27 23:56:01 +0300

    Update dump.sql

--- dump/dump.sql
+++ dump/dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `database` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `database`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: database
@@ -80,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-27 23:52:26
+-- Dump completed on 2023-03-27 23:55:56
